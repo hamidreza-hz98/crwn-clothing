2fcb7079b2aab31fb00cb89bf2b2999297ba7b95
unknown <hamidrezahz1999@gmail.com>
2022-02-21 00:20:20 +0330

    removed redux logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,11 @@ import logger from "redux-logger";
 import {persistStore} from 'redux-persist'
 import rootReducer from "./rootReducer";
 
-const middlewares=[logger]
+const middlewares=[]
+
+if(process.env.Node_ENV==='developement'){
+    middlewares.push(logger)
+}
 
 const store = createStore(rootReducer,applyMiddleware(...middlewares))
 
